967625c6482dc2ece675d6da407133c5c824895d
amanssit@gmail.com <19890900>
2016-11-21 12:36:40 +0530

    project ui ready with angular-fullstack

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -4,13 +4,13 @@ import routing from './main.routes';
 
 export class MainController {
   $http;
-
   awesomeThings = [];
   newThing = '';
 
   /*@ngInject*/
   constructor($http) {
     this.$http = $http;
+
   }
 
   $onInit() {
@@ -18,7 +18,98 @@ export class MainController {
       .then(response => {
         this.awesomeThings = response.data;
       });
+    myMap();
+
   }
+
+  myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(26.912484, 75.747331), zoom: 13
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+
+  var mylatLong1 = new google.maps.LatLng(26.912484, 75.747331);
+  setLocation1(mylatLong1, map);
+
+  var mylatLong2 = new google.maps.LatLng(26.8921609, 75.8155296);
+  setLocation2(mylatLong2, map);
+
+}
+
+  setLocation1(mylatLong, map) {
+  var myLocation = new google.maps.Marker({
+    position: mylatLong,
+    map: map,
+    title: "Me",
+    zIndex: 1,
+
+  });
+  // markers.push(myLocation);
+  var contentString = '<div class="event_one">' +
+    '<div class="event_item">' +
+    '<img src="assets/images/event_1.jpg">' +
+    '<div class="event_desc">' +
+    '<h6>Musical Night</h6>' +
+    '<p>Nucleya, Indian Ocean, Dualist Inquiry, Ankur & The Ghalat Family, Prateek Kuhad Live</p>' +
+    '<a href="javascript:;" class="btn btn-event">Know More</a>' +
+    '<a href="javascript:;" class="btn btn-event">Book Now</a>' +
+    '</div>' +
+    '</div>' +
+    '</div>';
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  google.maps.event.addListener(myLocation, 'click', function () {
+    infowindow.open(map, myLocation);
+    $(".gm-style-iw").parent().addClass("transparentClass");
+    $(".gm-style-iw").parent().children(':first-child').children().addClass('hideClass');
+  });
+
+  google.maps.event.addListener(myLocation, 'dragend', function (event) {
+    console.log(this.getPosition().lat());
+    console.log(this.getPosition().lng());
+  });
+
+}
+   setLocation2(mylatLong, map) {
+  var myLocation = new google.maps.Marker({
+    position: mylatLong,
+    map: map,
+    title: "Me",
+    zIndex: 1,
+
+  });
+  // markers.push(myLocation);
+  var contentString = '<div class="event_one">' +
+    '<div class="event_item">' +
+    '<img src="assets/images/event_1.jpg">' +
+    '<div class="event_desc">' +
+    '<h6>Musical Night</h6>' +
+    '<p>Nucleya, Indian Ocean, Dualist Inquiry, Ankur & The Ghalat Family, Prateek Kuhad Live</p>' +
+    '<a href="javascript:;" class="btn btn-event">Know More</a>' +
+    '<a href="javascript:;" class="btn btn-event">Book Now</a>' +
+    '</div>' +
+    '</div>' +
+    '</div>';
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  google.maps.event.addListener(myLocation, 'click', function () {
+    infowindow.open(map, myLocation);
+    $(".gm-style-iw").parent().addClass("transparentClass");
+    $(".gm-style-iw").parent().children(':first-child').children().addClass('hideClass');
+  });
+
+  google.maps.event.addListener(myLocation, 'dragend', function (event) {
+    console.log(this.getPosition().lat());
+    console.log(this.getPosition().lng());
+  });
+
+}
+
 
   addThing() {
     if(this.newThing) {
